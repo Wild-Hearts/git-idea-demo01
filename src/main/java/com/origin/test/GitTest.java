509447ca9e0a07004b186af27f9e34405b592947
origin <1223003136@qq.com>
2022-10-07 20:55:28 +0800

    first commit

---
+++ src/main/java/com/origin/test/GitTest.java
@@ -0,0 +1,10 @@
+package com.origin.test;
+
+/**
+ * @author origin
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello, world.");
+    }
+}
