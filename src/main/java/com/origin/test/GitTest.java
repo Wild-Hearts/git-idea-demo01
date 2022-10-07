25306c4962ed7f6ff37d9fbbc131b78e3fa53868
origin <1223003136@qq.com>
2022-10-07 21:12:00 +0800

    master-commit2

--- src/main/java/com/origin/test/GitTest.java
+++ src/main/java/com/origin/test/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("hello, world.");
         System.out.println("hello, world111.");
         System.out.println("hot-fix");
+        System.out.println("master-test");
     }
 }
