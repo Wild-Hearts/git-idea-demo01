416bce600131cf0864d77ab159b68e50410460a5
origin <1223003136@qq.com>
2022-10-07 21:07:29 +0800

    hot-fix01

--- src/main/java/com/origin/test/GitTest.java
+++ src/main/java/com/origin/test/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello, world.");
         System.out.println("hello, world111.");
+        System.out.println("hot-fix");
     }
 }
